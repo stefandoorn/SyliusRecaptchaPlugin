1ca8ae2d9a4f8d75461f2dad4993a521d91e7643
Joppe de Cuyper <joppe@yappa.be>
2019-10-03 19:36:42 +0200

    Added recaptcha to registration page

---
+++ src/Form/Extension/CustomerRegistrationFormExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace StefanDoorn\SyliusRecaptchaPlugin\Form\Extension;
+
+use EWZ\Bundle\RecaptchaBundle\Form\Type\EWZRecaptchaType;
+use EWZ\Bundle\RecaptchaBundle\Validator\Constraints\IsTrue as RecaptchaTrue;
+use Sylius\Bundle\CoreBundle\Form\Type\Customer\CustomerRegistrationType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class CustomerRegistrationFormExtension extends AbstractTypeExtension
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('recaptcha', EWZRecaptchaType::class, [
+            'mapped' => false,
+            'constraints' => [
+                new RecaptchaTrue([
+                    'groups' => 'sylius',
+                ]),
+            ],
+            'attr' => [
+                'defer' => true,
+                'async' => true,
+            ],
+        ]);
+    }
+
+    public function getExtendedType()
+    {
+        return CustomerRegistrationType::class;
+    }
+}
